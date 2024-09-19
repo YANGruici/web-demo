1783d14d118a7f4f67b673ad676dedf18e63adce
褚墨 <260980585@qq.com>
2024-09-19 16:07:03 +0800

    复制代码优化

--- src/utils/markdown.js
+++
@@ -1,8 +0,0 @@
-import {marked} from 'marked';
-
-export async function fetchMarkdown(fileName) {
-    const basePath = process.env.NODE_ENV === 'production' ? '/web-demo/' : '/';
-    const response = await fetch(`${basePath}markdown/${fileName}`);
-    const markdown = await response.text();
-    return marked(markdown);
-}
